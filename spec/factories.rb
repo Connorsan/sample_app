babccb182b9557fc8e0fd3599edb941e8cf52d63
connorsan <jconnors@actionls.com>
2013-04-20 14:08:49 -0500

    Finish user signup

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name  "Jake Connors"
-    email "jconnors@example.com"
+    email "example@railstutorial.com"
     password "teamrocket"
     password_confirmation "teamrocket"
   end
